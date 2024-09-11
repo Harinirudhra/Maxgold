348a4eb99ae3dc7ecdc88c3ab57faab95dc8da99
Hemachithra153 <127012186+Hemachithra153@users.noreply.github.com>
2024-09-11 18:19:57 +0530

    Update in Hero.jsx

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="bg-red-100 bg-cover bg-center h-screen flex items-center">
-            <img src='https://static.vecteezy.com/system/resources/previews/024/748/938/non_2x/confident-indian-beauty-exudes-sensuality-in-traditional-clothing-and-jewelry-generated-by-ai-free-photo.jpg' className='w-screen h-screen p-6'></img>
-            <div className="absolute left-20 top-2/4">
-                <h1 className="text-white text-6xl font-bold">You deserve valuable price in <span>market</span></h1>
-                <button className="bg-yellow-300 text-amber-900 font-bold py-2 px-4 rounded hover:bg-red-500 hover:text-white mx-6 my-8">
-                    Sell Gold
-                </button>
-                <button className="opacity-75 border border-white text-white font-bold py-2 px-4 rounded hover:bg-red-500  my-8">
-                    Know more
-                </button>
+        <div className="relative bg-red-100 bg-cover bg-center h-screen flex items-center">
+
+            <img 
+                src="/RKImages/RAMYA.jpg"
+                alt="hero" 
+                className="hidden lg:block w-full h-full p-4 rounded" />
+            
+    
+            <img
+                src="/RKImages/RAMYA MOBILE.jpg"
+                alt="RK"
+                className='block lg:hidden w-screen h-screen object-cover p-4 rounded'/>
+        
+
+            <div className="absolute inset-0 flex flex-col justify-start lg:justify-end lg:items-start items-center p-4 lg:p-20">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-white text-3xl md:text-3xl pt-3 lg:text-6xl font-bold">You deserve valuable price in</h1>
+                    <h1 className='text-white text-3xl md:text-3xl  lg:text-6xl font-bold'>market</h1>
+                </div>
+                <div className="flex flex-row float-start lg:flex-row gap-4 lg:gap-6 mt-4 lg:mt-8 p-6" >
+                    <button className="bg-yellow-300 text-amber-900 font-bold py-2 px-4 rounded hover:bg-red-500 hover:text-white">
+                        Sell Gold
+                    </button>
+                    <button className="bg-opacity-30 backdrop-blur-sm border border-white text-white font-bold py-2 px-4 rounded hover:bg-red-500">
+                        Know more
+                    </button>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default Hero;
