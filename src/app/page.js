c32b8566245f89d8bfc591cc2f4a7f1802ec399a
Srinivasan <113420841+Srini09git@users.noreply.github.com>
2024-09-11 10:40:14 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
+import Hero from "@/components/home/Hero";
+import HomeAbout from "@/components/home/HomeAbout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-      <h1>Hello maxgold</h1>
-
+    <div >
+      <Hero />
+      <HomeAbout />
     </div>
   );
 }
