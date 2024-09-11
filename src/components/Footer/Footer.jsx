7f9b78c00649592d5bf44b24a65534ac41e2bad9
Harini.R <harinirudhramurthy@gmail.com>
2024-09-11 14:40:24 +0530

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <div>
             <div className="bg-gradient-to-b from-red-400 via-red-500  py-12">
-                <div className="text-center text-white mb-8">
+                <div className="text-center text-white mb-8 h-52">
                     <h2 className="text-3xl font-bold">Are you ready to get started?</h2>
                     <p className="text-xl mt-2">We got you covered!</p>
                     <button className="mt-6 bg-white text-red-600 px-6 py-2 rounded-full text-lg font-bold">
@@ -25,64 +25,77 @@ const Footer = () => {
 
                             {/* Left Section */}
                             <div className="mb-8 md:mb-0">
-                                <h3 className="text-red-600 text-xl font-bold">Max Gold</h3>
+                                <h3 className="text-red-600 text-xl font-semibold">Max Gold</h3>
                                 <p className="mt-4 text-gray-600">Lorem ipsum dolor sit amet, consectetur</p>
                                 <p className="mt-2 text-gray-600"> adipiscing elit. Nullam dictum aliquet</p>
                                 <p className="mt-2 text-gray-600">   accumsan porta lectus ridiculus in mattis.</p>
                                 <p className="mt-2 text-gray-600">   Netus sodales in volutpat ullamcorper</p>
                                 <p className="mt-2 text-gray-600">     amet adipiscing fermentum.</p>
-                                <div className="flex space-x-4 mt-4">
-                                    <a href="#">< CiTwitter className="fab fa-twitter text-gray-600"/></a>
-                                    <a href="#">< FaFacebookF className="fab fa-facebook text-gray-600"/></a>
-                                    <a href="#"><FaInstagram className="fab fa-instagram text-gray-600"/></a>
-                                    <a href="#">< IoLogoGithub  className="fab fa-github text-gray-600"/></a>
+                                <div className="flex space-x-4 mt-9 mb-5">
+                                    <a href="#">< CiTwitter className="fab fa-twitter text-gray-600" /></a>
+                                    <a href="#">< FaFacebookF className="fab fa-facebook text-gray-600" /></a>
+                                    <a href="#"><FaInstagram className="fab fa-instagram text-gray-600" /></a>
+                                    <a href="#">< IoLogoGithub className="fab fa-github text-gray-600" /></a>
                                 </div>
                             </div>
 
                             {/* Company Section */}
                             <div>
-                                <h4 className="text-gray-900 font-bold">COMPANY</h4>
+                                <h4 className="text-red-400 font-bold">COMPANY</h4>
                                 <ul className="mt-4 space-y-2 text-gray-600">
-                                    <li className='mt-2'><a href="#">About</a></li>
-                                    <li className='mt-7'><a href="#">Features</a></li>
-                                    <li className='mt-7'><a href="#">Works</a></li>
-                                    <li className='mt-7'><a href="#">Career</a></li>
+                                    <li ><a href="#">About</a></li>
+                                    <li className='mt-10'><a href="#">Features</a></li>
+                                    <li className='mt-10'><a href="#">Works</a></li>
+                                    <li className='mt-10'><a href="#">Career</a></li>
                                 </ul>
                             </div>
 
                             {/* Resources Section */}
                             <div>
-                                <h4 className="text-gray-900 font-bold">RESOURCES</h4>
+                                <h4 className="text-red-400 font-bold ">RESOURCES</h4>
                                 <ul className="mt-4 space-y-2 text-gray-600">
-                                    <li><a href="#">Free eBooks</a></li>
-                                    <li><a href="#">Development Tutorial</a></li>
-                                    <li><a href="#">How to - Blog</a></li>
-                                    <li><a href="#">Youtube Playlist</a></li>
+                                    <li ><a href="#">Free eBooks</a></li>
+                                    <li className='mt-10'><a href="#">Development Tutorial</a></li>
+                                    <li className='mt-10'><a href="#">How to - Blog</a></li>
+                                    <li className='mt-10'><a href="#">Youtube Playlist</a></li>
                                 </ul>
                             </div>
 
                             {/* Contact Section */}
-                            <div>
+                            <div className='mr-20'>
+                          
                                 <ul className="mt-4 space-y-2 text-gray-600">
-                                    <li className="flex items-center">
-                                        <BsTelephone className="mr-2" />
-                                        Tel: 555-555-5555
+                                    <li className="flex items-start ">
+                                        <BsTelephone className="mr-2" /> {/* Icon */}
+                                        <div>
+                                            <span className="font-bold">Tel:</span> {/* Label */}
+                                            <div className='text-red-500 mt-2'>555-555-5555</div> {/* Content */}
+                                        </div>
                                     </li>
-                                    <li className="flex items-center">
-                                        <MdOutlineEmail className="mr-2" />
-                                        Mail: hi@magicdesign.io
+                                    <li className="flex items-start">
+                                        <MdOutlineEmail className="mr-2 " /> {/* Icon */}
+                                        <div>
+                                            <span className="font-bold">Mail:</span> {/* Label */}
+                                            <div  className='text-red-500 mt-2'>hi@magicdesign.io</div> {/* Content */}
+                                        </div>
                                     </li>
-                                    <li className="flex items-center">
-                                        <LuMapPin className="mr-2" />
-                                        Address: Wizard Ventures GmbH, Bugenhagenstr. 11, 10551 Berlin
+                                    <li className="flex items-start">
+                                        <LuMapPin className="mr-2" /> {/* Icon */}
+                                        <div>
+                                            <span className="font-bold ">Address:</span> {/* Label */}
+                                            <div  className='text-red-500  '>Wizard Ventures GmbH,</div>
+                                            <div  className='text-red-500  '>  Bugenhagenstr. 11,</div>
+                                            <div  className='text-red-500 '>    10551 Berlin</div> 
+                                        </div>
                                     </li>
                                 </ul>
+
                             </div>
                         </div>
 
                         {/* Bottom Links */}
-                        <div className="mt-12 flex flex-col md:flex-row justify-between text-gray-600 text-sm">
-                            <div className="flex space-x-4">
+                        <div className="mt-12 flex flex-col md:flex-row justify-between text-gray-600 text-sm mr-16">
+                            <div className="flex space-x-7">
                                 <a href="#">About us</a>
                                 <a href="#">Contact</a>
                                 <a href="#">Privacy policy</a>
