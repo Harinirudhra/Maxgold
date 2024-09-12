57646c088fb6cd1cf72bd9fc5deb9d20596424e0
Mohanapriya <148181118+MohanapriyaThirumurugan@users.noreply.github.com>
2024-09-12 13:54:22 +0530

    nav

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,83 +1,168 @@
 "use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import MenuIcon from '@mui/icons-material/Menu'; // Importing the Menu (hamburger) icon
-import CloseIcon from '@mui/icons-material/Close'; // Importing the Close (X) icon
-
+import React, { useState } from "react";
+import Link from "next/link";
+import MenuIcon from "@mui/icons-material/Menu"; // Importing the Menu (hamburger) icon
+import CloseIcon from "@mui/icons-material/Close"; // Importing the Close (X) icon
+import Image from "next/image"; // Importing Image component for the logo
+import maxigold from "../../../public/Logo/MaxGoldlogo.jpg";
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState('Home');
-
-  const handleNavClick = (tab) => {
-    setActiveTab(tab);
-    setIsOpen(false); // Close the menu on mobile after selecting a tab
-  };
+  const [isServicesOpen, setIsServicesOpen] = useState(false); // For dropdown
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  const tabs = [
-    { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
-    { name: 'Blog', href: '#blog' },
-    { name: 'Our Services', href: '#our-services' },
-    { name: 'Contact Us', href: '#contact-us' },
-  ];
+  const toggleServicesDropdown = () => {
+    setIsServicesOpen(!isServicesOpen); // Toggle services dropdown
+  };
 
   return (
-    <nav className="bg-gradient-to-r from-red-500 to-amber-900 p-6">
-
+    <nav className="bg-white p-6 shadow-md">
       <div className="flex justify-between items-center">
-        {/* Left Side: Max Gold */}
-        <div className="text-yellow-400 text-4xl font-bold">Max Gold</div>
+        {/* Left Side: Max Gold logo */}
+        <div className="flex items-center">
+          <Image src={maxigold} alt="Max Gold Logo" className="h-auto w-auto"/>
+        </div>
 
         {/* Hamburger Icon for small screens */}
         <div className="md:hidden" onClick={toggleMenu}>
           {isOpen ? (
-            <CloseIcon className="h-6 w-6 text-white" /> // Close icon when menu is open
+            <CloseIcon className="h-6 w-6 text-black" /> // Close icon when menu is open
           ) : (
-            <MenuIcon className="h-6 w-6 text-white" /> // Menu icon when menu is closed
+            <MenuIcon className="h-6 w-6 text-black" /> // Menu icon when menu is closed
           )}
         </div>
 
         {/* Desktop Navigation */}
-        <ul className="hidden md:flex md:space-x-6">
-          {tabs.map((tab) => (
-            <li key={tab.name}>
-              <Link href={tab.href}>
-                <span
-                  className={`cursor-pointer px-4 py-2 text-white block ${
-                    activeTab === tab.name ? 'text-yellow-200 underline' : ''
-                  }`}
-                  onClick={() => handleNavClick(tab.name)}
-                >
-                  {tab.name}
-                </span>
-              </Link>
-            </li>
-          ))}
+        <ul className="hidden md:flex md:space-x-6 text-xl">
+          {/* Home tab */}
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Home
+              </span>
+            </Link>
+          </li>
+
+          {/* About tab */}
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-800 hover:underline">
+                About
+              </span>
+            </Link>
+          </li>
+
+          {/* Blog tab */}
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Blog
+              </span>
+            </Link>
+          </li>
+
+          {/* Our Services tab with static dropdown */}
+          <li className="relative z-40">
+            <span
+              className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline"
+              onClick={toggleServicesDropdown}
+            >
+              Our Services <ArrowDropDownOutlinedIcon/>
+            </span>
+            {isServicesOpen && (
+              <ul className="absolute bg-white shadow-lg mt-2 w-40">
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 1
+                  </Link>
+                </li>
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 2
+                  </Link>
+                </li>
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 3
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+
+          {/* Contact Us tab */}
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Contact Us
+              </span>
+            </Link>
+          </li>
         </ul>
       </div>
 
       {/* Mobile Menu */}
       {isOpen && (
         <ul className="md:hidden flex flex-col space-y-2 mt-4">
-          {tabs.map((tab) => (
-            <li key={tab.name}>
-              <Link href={tab.href}>
-                <span
-                  className={`cursor-pointer px-4 py-2 text-white block ${
-                    activeTab === tab.name ? 'text-yellow-300 underline' : ''
-                  }`}
-                  onClick={() => handleNavClick(tab.name)}
-                >
-                  {tab.name}
-                </span>
-              </Link>
-            </li>
-          ))}
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                About
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Blog
+              </span>
+            </Link>
+          </li>
+          <li className="relative">
+            <span
+              className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline"
+              onClick={toggleServicesDropdown}
+            >
+              Our Services  <ArrowDropDownOutlinedIcon/>
+            </span>
+            {isServicesOpen && (
+              <ul className="mt-2">
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 1
+                  </Link>
+                </li>
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 2
+                  </Link>
+                </li>
+                <li className="hover:bg-gray-200">
+                  <Link href="#" className="block px-4 py-2 text-black">
+                    Service 3
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li>
+            <Link href="#">
+              <span className="cursor-pointer px-4 py-2 text-black block hover:text-red-700 hover:underline">
+                Contact Us
+              </span>
+            </Link>
+          </li>
         </ul>
       )}
     </nav>
