93ff5d20b138f7fd0c6733c8a52496e8545bc928
NANDHA KUMAR <nandhakumarnk2000@gmail.com>
2024-09-16 13:49:08 +0530

    Update Customers.jsx
    customer page done

--- src/components/about/Customers.jsx
+++ src/components/about/Customers.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div>
+<div class="flex items-center justify-center gap-x-1 p-6 sm:p-10 md:p-12 lg:p-16 xl:p-20bg-rose-300 sm:bg-white md:bg-rose-300 lg:bg-rose-300 xl:bg-rose-300">
+  <div class="sm:block -space-x-2 sm:-space-x-2">
+    <img class="inline-block h-10 w-10 sm:h-12 sm:w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+    <img class="inline-block h-10 w-10 sm:h-12 sm:w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+    <img class="inline-block h-10 w-10 sm:h-12 sm:w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2.25&amp;w=256&amp;h=256&amp;q=80" alt="" />
+    <img class="inline-block h-10 w-10 sm:h-12 sm:w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+    <img class="inline-block h-10 w-10 sm:h-12 sm:w-12 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" />
+  </div>
+  
+  <div class="border-none sm:border-l-2 sm:border-black sm:pl-0  md:ml-5">
+  <div class="flex ">
+   
+    <img class="w-4 sm:w-5" src="https://www.svgrepo.com/show/513354/star.svg" alt="stars-icon" />
+    <img class="w-4 sm:w-5" src="https://www.svgrepo.com/show/513354/star.svg" alt="stars-icon" />
+    <img class="w-4 sm:w-5" src="https://www.svgrepo.com/show/513354/star.svg" alt="stars-icon" />
+    <img class="w-4 sm:w-5" src="https://www.svgrepo.com/show/513354/star.svg" alt="stars-icon" />
+    <img class="w-4 sm:w-5" src="https://www.svgrepo.com/show/513354/star.svg" alt="stars-icon" />
+  </div>
+  <div>
+    <h3 class="text-xs font-medium sm:text-sm pt-1  ">Loved by 100+ Customers</h3>
+  </div>
+</div>
+
+
+
+
+</div>
+
+
+    </div>
   )
 }
 
