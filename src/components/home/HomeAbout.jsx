5d36abbcab2f3b81ee9c71fca9d630ac432640ec
rkraju0810 <rajeshtamil0810@gmail.com>
2024-09-17 13:37:20 +0530

    update on home about

--- src/components/home/HomeAbout.jsx
+++ src/components/home/HomeAbout.jsx
@@ -23,29 +23,31 @@ const cardData = [
 
 const AboutText = [
   {
-    heading1: "Welcome to Max Gold - Cash for Gold",
-    content1: "In the fast-paced world of gold transactions, Max Gold is the trusted name for individuals seeking a seamless and reliable solution to exchange their precious gold for cash. As a premier choice among gold buyers, strategically located in your vicinity, we excel in providing a swift and hassle-free process for selling gold and receiving cash in return. If you’ve ever wondered, “Where can I sell gold near me?” or “Where can I find the best cash-for-gold locations near me?” – the answer is simple: Max Gold.",
-    content2: "We serve as your go-to destination for selling gold and transforming your valuable items into cash. With years of experience catering to individuals eager to exchange their gold for cash in their local area, we prioritize not only the profitability of your transaction but also its safety and security.",
-    button: "read more",
+    heading: "Welcome to Max Gold - Cash for Gold",
+    // content: "In the fast-paced world of gold transactions, Max Gold is the trusted name for individuals seeking a seamless and reliable solution to exchange their precious gold for cash. As a premier choice among gold buyers, strategically located in your vicinity, we excel in providing a swift and hassle-free process for selling gold and receiving cash in return. If you’ve ever wondered, “Where can I sell gold near me?” or “Where can I find the best cash-for-gold locations near me?” – the answer is simple: Max Gold.",
+    // content: "We serve as your go-to destination for selling gold and transforming your valuable items into cash. With years of experience catering to individuals eager to exchange their gold for cash in their local area, we prioritize not only the profitability of your transaction but also its safety and security.",
+    content:["In the fast-paced world of gold transactions, Max Gold is the trusted name for individuals seeking a seamless and reliable solution to exchange their precious gold for cash. As a premier choice among gold buyers, strategically located in your vicinity, we excel in providing a swift and hassle-free process for selling gold and receiving cash in return. If you’ve ever wondered, “Where can I sell gold near me?” or “Where can I find the best cash-for-gold locations near me?” – the answer is simple: Max Gold.", "We serve as your go-to destination for selling gold and transforming your valuable items into cash. With years of experience catering to individuals eager to exchange their gold for cash in their local area, we prioritize not only the profitability of your transaction but also its safety and security."],
+    button: "Read more",
     reverse:false
   },
   {
-    heading2: "Max Gold - A Trusted Partner for Selling Gold",
-    content3: "We understand that parting with your gold can be a significant decision. Whether you’ve inherited jewellery that no longer serves your needs or you simply wish to convert your gold assets into cash, Max Gold is here to make the process as convenient and secure as possible. As prominent gold buyers near you, we take pride in our ability to offer a convenient and trustworthy solution to meet your needs.",
-    button: "read more",
+    heading: "Max Gold - A Trusted Partner for Selling Gold",
+    content: "We understand that parting with your gold can be a significant decision. Whether you’ve inherited jewellery that no longer serves your needs or you simply wish to convert your gold assets into cash, Max Gold is here to make the process as convenient and secure as possible. As prominent gold buyers near you, we take pride in our ability to offer a convenient and trustworthy solution to meet your needs.",
+    button: "Read more",
     reverse:true
   },
   {
     heading: "Selling Gold for Cash: A Hassle-Free process at Max Gold",
-    content: "Gold selling for cash should be a straightforward and stress-free experience, and that’s precisely what we provide at Max Gold. Our well-established and streamlined process ensures that you can quickly turn your gold into cash without unnecessary complications. We prioritize efficiency and ease in every step of the transaction, ensuring that you receive the best value for your gold.",
-    content4: "At Max Gold, we believe that selling gold for cash should be a hassle-free and straightforward experience, and we have meticulously designed our process to ensure just that. We understand that when individuals decide to part with their gold items, they want a seamless and efficient transaction that provides them with the best value. That’s precisely what you can expect when you choose Max Gold for your cash-for-gold needs.",
-    button: "read more",
+    // content: "Gold selling for cash should be a straightforward and stress-free experience, and that’s precisely what we provide at Max Gold. Our well-established and streamlined process ensures that you can quickly turn your gold into cash without unnecessary complications. We prioritize efficiency and ease in every step of the transaction, ensuring that you receive the best value for your gold.",
+    // content: "At Max Gold, we believe that selling gold for cash should be a hassle-free and straightforward experience, and we have meticulously designed our process to ensure just that. We understand that when individuals decide to part with their gold items, they want a seamless and efficient transaction that provides them with the best value. That’s precisely what you can expect when you choose Max Gold for your cash-for-gold needs.",
+    content: ["Gold selling for cash should be a straightforward and stress-free experience, and that’s precisely what we provide at Max Gold. Our well-established and streamlined process ensures that you can quickly turn your gold into cash without unnecessary complications. We prioritize efficiency and ease in every step of the transaction, ensuring that you receive the best value for your gold.","At Max Gold, we believe that selling gold for cash should be a hassle-free and straightforward experience, and we have meticulously designed our process to ensure just that. We understand that when individuals decide to part with their gold items, they want a seamless and efficient transaction that provides them with the best value. That’s precisely what you can expect when you choose Max Gold for your cash-for-gold needs."],
+    button: "Read more",
     reverse:false
   },
   {
     heading: "About Us",
     content: "In the fast-paced world of gold transactions, Max Gold is the trusted name for individuals seeking a seamless and reliable solution to exchange their precious gold for cash. As a premier choice among gold buyers, strategically located in your vicinity, we excel in providing a swift and hassle-free process for selling gold and receiving cash in return. If you’ve ever wondered, “Where can I sell gold near me?” or “Where can I find the best cash-for-gold locations near me?” – the answer is simple: Max Gold.We serve as your go-to destination for selling gold and transforming your valuable items into cash. With years of experience catering to individuals eager to exchange their gold for cash in their local area, we prioritize not only the profitability of your transaction but also its safety and security.",
-    button: "read more",
+    button: "Read more",
     reverse:true
   }
 ]
@@ -63,52 +65,42 @@ const HomeAbout = () => {
   return (
     <div>
       {/* Static Section */}
-      <div className="flex flex-col items-center sm:items-start sm:px-5 md:px-10 lg:px-20 ">
-
-        {AboutText.map((itemz, index) => (
-          <div className={`flex flex-col items-center justify-center ${itemz.reverse ? 'md:flex-row-reverse': 'md:flex-row'}`} key={index}>
-          <Image
+      <div className="flex flex-col items-center sm:items-start px-6 md:px-8 lg:px-10">
+  {AboutText.map((itemz, index) => (
+    <div
+      className={`flex flex-col items-start justify-center gap-4 ${itemz.reverse ? 'md:flex-row-reverse' : 'md:flex-row'}`}
+      key={index}
+      // style={{ maxHeight: '450px' }}  // Ensures max height is the same for both content and image
+    >
+      <div className="w-full sm:w-1/2 md:w-1/2 pt-5" style={{ height: '450px' }}>
+        <Image
           src={samplepic}
+          alt="Sample"
           width={700}
           height={100}
-          style={{ height: '450px' }}
-          className="p-3 w-full sm:w-1/2 md:w-1/2 lg:1/4 pt-7"
+          style={{ height: '100%' }}  // Ensures image height fills the container
+          className="w-full object-cover"  // Prevents overflow and maintains aspect ratio
         />
-          < div className="ml-0 sm:ml-5 md:ml-3 lg:ml-10 pr-5 pt-0 md:pr-0 lg:pr-2 mt-0 sm:mt-0 md:mt-0 sm:w-1/2 md:w-1/2 p-3">
-            <h1 className="text-red-600 pt-3 text-2xl sm:text-3xl md:text-3xl md:pt-1 lg:text-4xl font-bold lg:mt-5">{itemz.heading}</h1>
-            <h1 className="text-red-600 pt-3 text-2xl sm:text-3xl md:text-3xl md:pt-5 lg:text-4xl font-bold lg:mt-2">{itemz.heading1}</h1>
-            <p className=" text-gray-500 text-justify md:mt-1 lg:mt-3 xl:mt-3">
-              {itemz.content1}
-            </p>
-            <p className="mt-6 text-gray-500 text-justify md:mt-1 lg:mt-3 xl:mt-3">
-              {itemz.content2}
-            </p>
-            
-
-
-
-            <h1 className="text-red-600 pt-0 text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold lg:mt-0">{itemz.heading2}</h1>
-            <p className="mt-6 text-gray-500 text-justify md:mt-1 lg:mt-6 xl:mt-10">
-              {itemz.content3}
-            </p>
-            
-           
-            
-            
-            <p className="mt-6 text-gray-500 text-justify md:mt-1 lg:mt-3 xl:mt-3">
-              {itemz.content}
-            </p>
-            
-
-            <button className="mt-0 text-gray-500 text-sm border rounded-3xl border-pink-500 p-2 px-7 hover:bg-red-500 hover:text-white transition duration-300 md:mt-0 lg:mt-5 xl:mt-5">
-              {itemz.button}
-            </button>
-            
-         
-          </div>
-          </div>
-          ))}
       </div>
+      <div className="w-full pt-5 sm:w-1/2 md:w-1/2 flex flex-col gap-4 justify-between">
+        <h1 className="text-red-600">{itemz.heading}</h1>
+        {Array.isArray(itemz.content) ? (
+          itemz.content.map((liitem, index) => (
+            <p className="text-gray-500" key={index}>
+              {liitem}
+            </p>
+          ))
+        ) : (
+          <p className="text-gray-500">{itemz.content}</p>
+        )}
+        <button className="w-40 text-gray-500 text-sm border rounded-3xl border-pink-500 p-2 hover:bg-red-500 hover:text-white transition duration-300">
+          {itemz.button}
+        </button>
+      </div>
+    </div>
+  ))}
+</div>
+
 
       {/* Reusable Cards Section */}
       <div className="flex flex-col md:flex-row gap-x-7 gap-y-10 sm:ml-10 p-5 md:ml-10 mr-7 mt-10 mb-10 lg:ml-20">
