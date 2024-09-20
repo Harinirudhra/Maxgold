0da48147a5b5ff53109103a10eb36bc88aab0cce
Hemachithra153 <127012186+Hemachithra153@users.noreply.github.com>
2024-09-20 13:02:55 +0530

    Update in ServicesHero.jsx

--- src/components/services/ServicesHero.jsx
+++ src/components/services/ServicesHero.jsx
@@ -10,7 +10,7 @@ const ServicesHero = ({image}) => {
 const Hero = () => {
   return(
     <ServicesHero
-    image = "https://img.freepik.com/free-photo/elegant-gemstone-necklace-shines-with-rare-sophistication-generated-by-ai_188544-10736.jpg?ga=GA1.1.8839009.1724129443&semt=ais_hybrid"
+    image = "https://i.pinimg.com/originals/ff/9c/20/ff9c204f62b65141a988cde3c7b1484f.jpg"
     />
   )
 }
