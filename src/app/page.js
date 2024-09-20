17440821148b9f5626b3bba07e74c82c4372db58
Harini.R <harinirudhramurthy@gmail.com>
2024-09-20 14:15:01 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import HomeAbout from "@/components/home/HomeAbout";
 import MaxGoldADS from "@/components/home/MaxGoldADS";
 import WhyMaxGold from "@/components/home/WhyMaxGold";
 import "./globals.css";
+import OurValue from "@/components/about/OurValue";
+import Testimonials from "@/components/about/Testimonials";
+import Aboutus from "@/components/services/Aboutus";
 
 export default function Home() {
   return (
@@ -10,6 +13,8 @@ export default function Home() {
       <Hero />
       <HomeAbout />
       <WhyMaxGold/>
+      <OurValue/>
+      <Testimonials/>
       <MaxGoldADS/>
     </div>
   );
