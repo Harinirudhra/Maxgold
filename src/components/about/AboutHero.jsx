37e669238c2922d4a69aa754f10715e319725ca0
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2024-09-30 15:22:45 +0530

    do

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -3,29 +3,13 @@ import React from 'react';
 const AboutHero = ({ title, description, imageurl }) => {
   return (
     <div className="relative">
-      
       <img src={imageurl} className="object-cover w-screen h-screen" alt="hero background" />
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
-      {/* <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mx-14 ">
-      <h1 className="bg-gradient-to-r from-[#f44336] to-[#FFCE00] bg-clip-text text-transparent lg:text-9xl text-6xl lg:py-3">
-  {title}
-</h1>
-  <p className="text-white lg:text-3xl text-2xl lg:w-2/4">{description}</p>
-</div> */}
-{/* #f44336 */}
-{/* #FFCE00 */}
-<div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[-85px] overflow-hidden">
-  <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">
-    {title}
-  </h1>
-  <p className="text-white lg:text-3xl text-2xl lg:w-2/5 lg:ml-14">
-    {description}
-  </p>
-</div>
-
-
+      
+      <div className="absolute lg:top-0 lg:left-0 lg:mt-20 top-0 left-0 p-4">
+        <h1 className="text-yellow-300 lg:text-7xl text-5xl lg:-mt-8 lg:p-14 mt-4 p-4">{title}</h1>
+        <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">{description}</p>
+      </div>
     </div>
-  
   );
 };
 
