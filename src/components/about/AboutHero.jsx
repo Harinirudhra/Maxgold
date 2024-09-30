45c9f63d067d7cb8bc1fc0c93d98d307f02313b8
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2024-09-30 15:45:32 +0530

    done

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -4,11 +4,20 @@ const AboutHero = ({ title, description, imageurl }) => {
   return (
     <div className="relative">
       <img src={imageurl} className="object-cover w-screen h-screen" alt="hero background" />
-      
-      <div className="absolute lg:top-0 lg:left-0 lg:mt-20 top-0 left-0 p-4">
-        <h1 className="text-yellow-300 lg:text-7xl text-5xl lg:-mt-8 lg:p-14 mt-4 p-4">{title}</h1>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+      {/* <div className="absolute  flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[85px]">
+        <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">{title}</h1>
         <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">{description}</p>
-      </div>
+      </div> */}
+      <div className="absolute top-0 flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[280px]  lg:gap-8 ">
+  <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">
+    {title}
+  </h1>
+  <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">
+    {description}
+  </p>
+</div>
+
     </div>
   );
 };
