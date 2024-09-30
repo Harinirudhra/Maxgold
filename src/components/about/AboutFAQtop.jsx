368d8ab88ebd224091e5cd00b9f65b4026254cfc
Kasinathan22 <89632353+Kasinathan22@users.noreply.github.com>
2024-09-30 15:52:10 +0530

    a

--- src/components/about/AboutFAQtop.jsx
+++ src/components/about/AboutFAQtop.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 const AboutFAQtop = () => {
   return (
-    <div>
-        <h1>
-            FAQ
-            </h1>
+    <div className='w-auto h-auto'>
+    <div className=' justify-center items-center'>
+        <div className='flex flex-col items-center'> {/* Add this div to center the heading */}
+            <h1 className='text-center py-5 md:lg:py-10 font-bold'>Frequently Asked Questions</h1>
+        </div>
+        <div className='px-10 md:lg:px-16 '>
+            <img src='/Faq/faq.jpg' alt="FAQ" className='rounded-2xl'/>
+        </div>
     </div>
+</div>
+
   )
 }
 
