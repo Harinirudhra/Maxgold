5fdafdf1abf7ca03b79b51ee5dc2c9ae992cdaa2
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2024-09-30 16:01:01 +0530

    done

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -10,11 +10,11 @@ const AboutHero = ({ title, description, imageurl }) => {
         <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">{description}</p>
       </div> */}
 
-<div className="absolute top-1/2 lg:top-0 left-0 w-full transform -translate-y-1/2 lg:translate-y-0 flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[280px] lg:gap-8">
+<div className="absolute top-1/2 lg:top-0 left-0 w-full transform -translate-y-1/2 lg:translate-y-0 flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[300px] lg:gap-8">
   <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">
     {title}
   </h1>
-  <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">
+  <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-2/3">
     {description}
   </p>
 </div>
