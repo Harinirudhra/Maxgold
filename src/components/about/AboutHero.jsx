828c1cf66215ee184dcdc57a3a5c047421ebb42f
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2024-10-01 10:57:35 +0530

    hero

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -5,16 +5,12 @@ const AboutHero = ({ title, description, imageurl }) => {
     <div className="relative">
       <img src={imageurl} className="object-cover w-screen h-screen" alt="hero background" />
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
-      {/* <div className="absolute top-0 flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[280px] lg:gap-8">
-        <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">{title}</h1>
-        <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-96">{description}</p>
-      </div> */}
 
-<div className="absolute top-1/2 lg:top-0 left-0 w-full transform -translate-y-1/2 lg:translate-y-0 flex flex-col items-center justify-center lg:items-start lg:justify-end text-center lg:text-left p-4 lg:mt-[300px] lg:gap-8">
-  <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent lg:text-9xl text-6xl py-3 lg:ml-14">
+<div className="absolute top-1/2 lg:top-0 left-0 w-full transform -translate-y-1/2 lg:translate-y-0 flex flex-col items-center justify-center md:items-start md:justify-end md:text-left lg:items-start lg:justify-end text-center lg:text-left p-4 md:mt-[300px] lg:mt-[300px] lg:gap-8 xl:mt-[350px] xl:gap-10">
+  <h1 className="bg-gradient-to-r from-[#ff2c05] to-[#fef001] bg-clip-text text-transparent md:lg:text-9xl md:text-8xl text-6xl py-3 lg:ml-14 md:ml-3">
     {title}
   </h1>
-  <p className="text-white lg:text-3xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-2/3">
+  <p className="text-white lg:text-3xl md:text-2xl text-2xl ml-5 lg:ml-14 lg:-mt-8 mt-4 lg:font-bold md:w-2/3">
     {description}
   </p>
 </div>
