80f02fbeb8b5533320ac34bdc6ea2a1772b6d9e6
Srinivasan <113420841+Srini09git@users.noreply.github.com>
2024-10-04 14:13:03 +0530

    done

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -5,9 +5,8 @@ import Aboutus from '@/components/services/Aboutus'
 import ServicesHero from '@/components/services/ServicesHero'
 import React from 'react'
 import Ourservices from '@/components/services/Ourservices'
-import Extra2 from '@/components/services/Extra2'
-import Extra3 from '@/components/services/Extra3'
-import Extra1 from '@/components/home/Extra1'
+import Parallax from '@/components/about/Parallex'
+
 
 const page = () => {
   return (
@@ -15,11 +14,10 @@ const page = () => {
       <ServicesHero />
       <Ourservices />
       <Servicegrid />
+      <Parallax />
       <Aboutus />
 
       <Achievesucesses />
-      <Extra3 />
-      <Extra2 />
 
 
     </div>
