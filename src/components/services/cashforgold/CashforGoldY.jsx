1d61259b6e5b896adef49fb6cc94c6eac5841f67
Harini.R <harinirudhramurthy@gmail.com>
2024-10-18 11:36:38 +0530

    done

--- src/components/services/cashforgold/CashforGoldY.jsx
+++ src/components/services/cashforgold/CashforGoldY.jsx
@@ -2,9 +2,85 @@ import React from 'react'
 
 const CashforGoldY = () => {
     return (
-        <div>
-            CashforGoldY
+        <section class="">
+    <div class="py-12 bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+            <div class="lg:text-center">
+                <h2
+                    class="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
+                    Why choose us?
+                </h2>
+                <p class="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
+                Understanding the Value of Your Gold at Max Gold
+                </p>
+                
+            </div>
+
+            <div class="mt-10">
+                <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    <div class="relative">
+                        <dt>
+                            <div
+                                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                                {/* <img src="https://www.svgrepo.com/show/503163/api-settings.svg" /> */}
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Purity and Karat</p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500">
+                        Gold jewelry is often marked with a karat value, which indicates the purity of the gold used.
+                         Karat values typically range from 24k
+                         (pure gold) to 10k. A higher karat signifies purer gold and a more valuable item.
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div
+                                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                                {/* <img src="https://www.svgrepo.com/show/503138/webpack.svg" /> */}
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Weight</p>
+                            
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> The weight of your gold jewelry, measured in grams, plays a pivotal role in
+                             determining its value. Heavier items contain more gold and, consequently, 
+                             command a higher value
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div
+                                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                                {/* <img src="https://www.svgrepo.com/show/511771/dashboard-671.svg" /> */}
+
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Market Price
+                            </p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> The current market price of gold is a critical factor influencing the value of your gold items. Gold prices fluctuate daily in response to global supply and demand, economic conditions,
+                             and geopolitical events. Selling your gold when prices are favorable can maximize your returns.
+                        </dd>
+                    </div>
+                    <div class="relative">
+                        <dt>
+                            <div
+                                class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                                {/* <img src="https://www.svgrepo.com/show/76267/free-commercial-label.svg" /> */}
+
+                            </div>
+                            <p class="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Design and Craftsmanship
+                            </p>
+                        </dt>
+                        <dd class="mt-2 ml-16 text-base text-gray-500"> The design, craftsmanship, and aesthetics of your gold jewelry can also affect its value.
+                             Antique or designer pieces may have additional value beyond their gold content.
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+
         </div>
+    </div>
+</section>
     )
 }
 
