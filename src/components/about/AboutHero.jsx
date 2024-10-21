662c58025131fff9770357d6ab0f90af45a6bab6
Simbusam <naveen.weboin@gmail.com>
2024-10-21 12:13:04 +0530

    added animations to aboutus

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -1,19 +1,45 @@
+'use client';
 import React from 'react';
 
 const AboutHero = ({ title, description, imageurl }) => {
   return (
-    <div className="relative flex justify-center text-center ">
+    <div className="relative flex justify-center text-center">
       <img src={imageurl} className="object-cover w-screen h-screen" alt="hero background" />
-      <div className="absolute  left-0 w-full h-full bg-black opacity-40"></div>
+      <div className="absolute left-0 w-full h-full bg-black opacity-40"></div>
 
-     <div className="absolute justify-center items-center top-36 md:top-56 p-8 lg:w-1/3">
-       <h1 className="text-white bg-clip-text text-7xl pb-4">
-    {title}
-  </h1>
-  <p className="text-white text-3xl ">
-    {description}
-  </p>
-</div>
+      <div className="absolute justify-center items-center top-36 md:top-56 p-8 lg:w-1/3">
+        <h1 className="text-white bg-clip-text text-7xl pb-4 animate-fade-up">
+          {title}
+        </h1>
+        <p className="text-white text-3xl animate-fade-up">
+          {description}
+        </p>
+      </div>
+      
+      <style jsx>{`
+        @keyframes fadeInUp {
+          0% {
+            opacity: 0;
+            transform: translateY(20px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+
+        .animate-fade-up {
+          animation: fadeInUp 0.7s ease-out forwards;
+        }
+
+        .animate-fade-up:nth-child(1) {
+          animation-delay: 0.2s; /* Delay for the h1 */
+        }
+
+        .animate-fade-up:nth-child(2) {
+          animation-delay: 0.2s; /* Delay for the p */
+        }
+      `}</style>
     </div>
   );
 };
