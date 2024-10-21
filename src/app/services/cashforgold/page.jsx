89ff5772cea95b6b798c54fb52a2a52dc3131d2a
Harini.R <harinirudhramurthy@gmail.com>
2024-10-21 16:05:51 +0530

    done

--- src/app/services/cashforgold/page.jsx
+++ src/app/services/cashforgold/page.jsx
@@ -2,6 +2,7 @@ import Parallax from '@/components/about/Parallex'
 import CashforgoldX from '@/components/services/cashforgold/CashforgoldX'
 import CashforGoldY from '@/components/services/cashforgold/CashforGoldY'
 import CashforgoldZ from '@/components/services/cashforgold/CashforgoldZ'
+import ReleaseGoldX from '@/components/services/releasegold/ReleaseGoldX'
 import Servicegrid from '@/components/services/Servicegrid'
 import React from 'react'
 
@@ -9,8 +10,8 @@ const page = () => {
     return (
         <div>
              <CashforgoldX />
-            
-            <Parallax/>
+             <CashforGoldY/>
+           
             <CashforgoldZ/>
            
         </div>
