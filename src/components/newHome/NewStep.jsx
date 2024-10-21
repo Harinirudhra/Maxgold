a53a8e1496903e4b70e3ed7c41c64e741963d939
Simbusam <naveen.weboin@gmail.com>
2024-10-21 10:54:49 +0530

    added animation to new step

--- src/components/newHome/NewStep.jsx
+++ src/components/newHome/NewStep.jsx
@@ -1,8 +1,9 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneAlt, faCalculator, faCheck, faStore, faHome, faSmile } from '@fortawesome/free-solid-svg-icons';
+import ScrollTrigger from 'react-scroll-trigger';
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
@@ -20,71 +21,79 @@ const container = {
 };
 
 const NewStep = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleEnterViewport = () => {
+    setScrolling(true);
+  };
+
   return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      variants={container}
-      className='px-10 bg-slate-200'>
+    <ScrollTrigger onEnter={handleEnterViewport}>
+      <motion.div
+        initial="hidden"
+        animate={scrolling ? "visible" : "hidden"}
+        variants={container}
+        className='px-10 bg-slate-200'>
 
-      <motion.h1
-        variants={fadeInUp}
-        className='pt-5 text-3xl font-bold text-center text-[#efbf04]'
-      >
-        Steps to sell your old gold
-      </motion.h1>
-
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
-        {/* Left Image Section */}
-        <motion.div
+        <motion.h1
           variants={fadeInUp}
-          className='flex justify-center items-center h-full'
+          className='pt-5 text-3xl font-bold text-center text-[#efbf04]'
         >
-          <motion.img
-            src="/rbg.png"
-            alt="img"
-            className='object-cover w-full h-full'
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1, transition: { duration: 0.8 } }}
-          />
-        </motion.div>
+          Steps to sell your old gold
+        </motion.h1>
 
-        {/* Right Content Section */}
-        <motion.div
-          className='relative space-y-10'
-          initial="hidden"
-          animate="visible"
-          variants={container}
-        >
-          {[
-            { number: "1", title: "Contact Us", text: "To sell your gold, reach out to us at 97501 97501 or via WhatsApp to arrange doorstep service.", icon: faPhoneAlt },
-            { number: "2", title: "Get Quote", text: "Our expert team provides a comprehensive estimate based on current market rates for your gold, silver, and diamond jewelry.", icon: faCalculator },
-            { number: "3", title: "Transparent Service", text: "Our estimation value remains the highest in Chennai. We offer transparent service throughout the selling process.", icon: faCheck },
-            { number: "4", title: "Visit Our Branch", text: "Visit any of our branches to sell your gold for the same estimated value offered by our team.", icon: faStore },
-            { number: "5", title: "Doorstep Service", text: "Our friendly staff will visit your home, verify the gold, and make payments accordingly.", icon: faHome },
-            { number: "6", title: "100% Customer Satisfaction", text: "Your satisfaction is our paramount concern. We strive to offer the highest price while ensuring a secure process.", icon: faSmile },
-          ].map((section, index) => (
-            <motion.div key={index} variants={fadeInUp} className="flex items-center space-x-4">
-              <div className="relative flex items-center justify-center h-16 w-16 rounded-full bg-[#7b071e] z-10">
-                <FontAwesomeIcon icon={section.icon} className="text-white text-2xl p-8" />
-              </div>
-              <span className='text-4xl hidden lg:flex font-extrabold text-[#7b071e]'>{section.number} </span>
-              <div className="space-y-2 pl-4">
-                <h1 className='font-bold text-2xl text-[#7b071e]'>
-                  <span className='text-2xl lg:hidden text-[#7b071e]'>{section.number} </span> {section.title}
-                </h1>
-                <p>{section.text}</p>
-              </div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
+          {/* Left Image Section */}
+          <motion.div
+            variants={fadeInUp}
+            className='flex justify-center items-center h-full'
+          >
+            <motion.img
+              src="/rbg.png"
+              alt="img"
+              className='object-cover w-full h-full'
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1, transition: { duration: 0.8 } }}
+            />
+          </motion.div>
 
-              {/* Add small vertical line */}
-              {index < 5 && (
-                <div className="absolute left-4 md:top-full md:h-10 w-1 bg-black items-center justify-center"></div>
-              )}
-            </motion.div>
-          ))}
-        </motion.div>
-      </div>
-    </motion.div>
+          {/* Right Content Section */}
+          <motion.div
+            className='relative space-y-10'
+            initial="hidden"
+            animate={scrolling ? "visible" : "hidden"}
+            variants={container}
+          >
+            {[
+              { number: "1", title: "Contact Us", text: "To sell your gold, reach out to us at 97501 97501 or via WhatsApp to arrange doorstep service.", icon: faPhoneAlt },
+              { number: "2", title: "Get Quote", text: "Our expert team provides a comprehensive estimate based on current market rates for your gold, silver, and diamond jewelry.", icon: faCalculator },
+              { number: "3", title: "Transparent Service", text: "Our estimation value remains the highest in Chennai. We offer transparent service throughout the selling process.", icon: faCheck },
+              { number: "4", title: "Visit Our Branch", text: "Visit any of our branches to sell your gold for the same estimated value offered by our team.", icon: faStore },
+              { number: "5", title: "Doorstep Service", text: "Our friendly staff will visit your home, verify the gold, and make payments accordingly.", icon: faHome },
+              { number: "6", title: "100% Customer Satisfaction", text: "Your satisfaction is our paramount concern. We strive to offer the highest price while ensuring a secure process.", icon: faSmile },
+            ].map((section, index) => (
+              <motion.div key={index} variants={fadeInUp} className="flex items-center space-x-4">
+                <div className="relative flex items-center justify-center h-16 w-16 rounded-full bg-[#7b071e] z-10">
+                  <FontAwesomeIcon icon={section.icon} className="text-white text-2xl p-8" />
+                </div>
+                <span className='text-4xl hidden lg:flex font-extrabold text-[#7b071e]'>{section.number} </span>
+                <div className="space-y-2 pl-4">
+                  <h1 className='font-bold text-2xl text-[#7b071e]'>
+                    <span className='text-2xl lg:hidden text-[#7b071e]'>{section.number} </span> {section.title}
+                  </h1>
+                  <p>{section.text}</p>
+                </div>
+
+                {/* Add small vertical line */}
+                {index < 5 && (
+                  <div className="absolute left-4 md:top-full md:h-10 w-1 bg-black items-center justify-center"></div>
+                )}
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </motion.div>
+    </ScrollTrigger>
   );
 };
 
