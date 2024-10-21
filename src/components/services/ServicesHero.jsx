e81e2b381eb12d470be56ca38ffd7220b7d73871
Simbusam <naveen.weboin@gmail.com>
2024-10-21 14:47:12 +0530

    changed the text title of service hero

--- src/components/services/ServicesHero.jsx
+++ src/components/services/ServicesHero.jsx
@@ -1,23 +1,60 @@
+
+'use client';
 import React from 'react';
 
-const ServicesHero = () => {
-  const image = "/gold2.jpg"; // Image path
-  const title = "Turning Treasures into Trust"; // Title text
-  const description = "Transform your gold into cash, ensuring value and fostering trust. Every transaction helps uplift both you and our community."; // Description text
-
+const ServicesHeroz = ({ title, description, imageurl }) => {
   return (
-    <div className='flex sm:justify-center min-h-screen lg:justify-start lg:items-start relative'> {/* Added relative positioning */}
-      <img src={image} alt={title} className='w-screen h-screen object-cover' />
-      <div className='absolute lg:ml-6 text-white lg:w-1/3'>
-        <h1 className='lg:text-6xl md:text-4xl text-2xl flex justify-center pt-16'>
-          {title}
-        </h1>
-        <p className='text-xl lg:text-3xl flex font-bold flex-wrap lg:pt-10 pt-4 px-4 justify-center md:text-center lg:text-left md:px-10 lg:px-0'>
-          {description}
-        </p>
-      </div>
+    <div className="relative flex justify-center text-center">
+      <img src={imageurl} className="object-cover w-screen h-screen" alt="hero background" />
+      <div className="absolute left-0 w-full h-full bg-black opacity-40"></div>
+
+      <div className="absolute justify-center items-center top-36 md:top-30 p-8 lg:w-1/3">
+    <h1 className="text-white bg-clip-text text-7xl pb-4 animate-fade-up">
+        {title}
+    </h1>
+    <p className="text-white text-2xl animate-fade-up w-full md:w-3/4 lg:w-full">
+        {description}
+    </p>
+</div>
+
+      
+      <style jsx>{`
+        @keyframes fadeInUp {
+          0% {
+            opacity: 0;
+            transform: translateY(20px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+
+        .animate-fade-up {
+          animation: fadeInUp 0.7s ease-out forwards;
+        }
+
+        .animate-fade-up:nth-child(1) {
+          animation-delay: 0.2s; /* Delay for the h1 */
+        }
+
+        .animate-fade-up:nth-child(2) {
+          animation-delay: 0.2s; /* Delay for the p */
+        }
+      `}</style>
     </div>
   );
 };
 
+const ServicesHero = () => {
+  return (
+    <ServicesHeroz
+      title="Turning Treasures into Trust"
+      description="Transform your gold into cash, ensuring value and fostering trust. Every transaction helps uplift both you and our community"
+      imageurl="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2f73bb61658493.5a75732e670c8.jpg"
+    />
+  );
+};
+
 export default ServicesHero;
+
