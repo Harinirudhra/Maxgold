b2f75667e30c9b1b389a8342a38c617935a46ffb
Srinivasan <113420841+Srini09git@users.noreply.github.com>
2024-10-21 12:20:25 +0530

    done

--- src/components/services/releasegold/ReleaseGoldY.jsx
+++ src/components/services/releasegold/ReleaseGoldY.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const ReleaseGoldY = () => {
     return (
         <div>
-            <div class="relative overflow-hidden bg-[#550000] pt-16 pb-32 space-y-24">
+            <div class="relative overflow-hidden bg-[#7b071e] pt-16 pb-32 space-y-24">
                 <div class="relative">
                     <div class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8 ">
                         <div class="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 ">
@@ -10,30 +10,30 @@ const ReleaseGoldY = () => {
                             <div>
                                 <div class="-mt-9">
                                     <h2 class="text-3xl font-bold tracking-tight  text-[#FFD700]">
-                                    Repay the Loan:
+                                        Repay the Loan:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300 ">
-                                    The first and most crucial step in releasing pledged gold is to repay the loan or credit for which you pledged the gold. Ensure that you’ve fulfilled all financial obligations associated with the loan, including interest, principal, and any additional fees.
+                                        The first and most crucial step in releasing pledged gold is to repay the loan or credit for which you pledged the gold. Ensure that you’ve fulfilled all financial obligations associated with the loan, including interest, principal, and any additional fees.
                                     </p>
-                                   
+
                                 </div>
                                 <div class="mt-6">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Contact the Lender:
+                                        Contact the Lender:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    Once the loan is fully repaid, you should contact the lender to initiate the release process. Notify them of your intention to release the pledged gold and inquire about the specific procedures and requirements.
+                                        Once the loan is fully repaid, you should contact the lender to initiate the release process. Notify them of your intention to release the pledged gold and inquire about the specific procedures and requirements.
                                     </p>
-                                   
+
                                 </div>
                                 <div class="mt-6">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Prepare the Necessary Documents:
+                                        Prepare the Necessary Documents:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    Most lenders will require you to provide specific documents to facilitate the release of pledged gold. These documents may include a loan closure letter, a release request form, and proof of repayment, such as a loan statement or payment receipt.
+                                        Most lenders will require you to provide specific documents to facilitate the release of pledged gold. These documents may include a loan closure letter, a release request form, and proof of repayment, such as a loan statement or payment receipt.
                                     </p>
-                                   
+
                                 </div>
                             </div>
                         </div>
@@ -57,33 +57,33 @@ const ReleaseGoldY = () => {
                         <div class="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 lg:col-start-2">
                             <div>
                                 <div>
-                                  
+
                                 </div>
                                 <div class="mt-6">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Visit the Lender:
+                                        Visit the Lender:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    In many cases, you will need to visit the lender’s office or branch in person to complete the release process. It’s advisable to call ahead and schedule an appointment to ensure that a representative is available to assist you.
+                                        In many cases, you will need to visit the lender’s office or branch in person to complete the release process. It’s advisable to call ahead and schedule an appointment to ensure that a representative is available to assist you.
                                     </p>
                                 </div>
                                 <div class="mt-14">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Appraisal of the Pledged Gold:
+                                        Appraisal of the Pledged Gold:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    Before the release, the lender may appraise the pledged gold to confirm its authenticity and value. This appraisal is typically conducted by a certified and impartial appraiser.
+                                        Before the release, the lender may appraise the pledged gold to confirm its authenticity and value. This appraisal is typically conducted by a certified and impartial appraiser.
                                     </p>
                                 </div>
                                 <div class="mt-14">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Settlement of Any Outstanding Chargesr:
+                                        Settlement of Any Outstanding Chargesr:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    It’s essential to ensure that there are no outstanding charges or fees associated with the loan. Clear any remaining dues to avoid any delays in the release process.
+                                        It’s essential to ensure that there are no outstanding charges or fees associated with the loan. Clear any remaining dues to avoid any delays in the release process.
                                     </p>
-                                
-                                   
+
+
                                 </div>
                             </div>
                         </div>
@@ -107,34 +107,34 @@ const ReleaseGoldY = () => {
                         <div class="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 ">
                             <div>
                                 <div>
-                                 
+
                                 </div>
                                 <div class="-mt-9">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Sign Release Documents:
+                                        Sign Release Documents:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    Once all conditions are met, you will be asked to sign the release documents, which acknowledge the return of your pledged gold. These documents serve as legal proof of the release and ownership transfer.
+                                        Once all conditions are met, you will be asked to sign the release documents, which acknowledge the return of your pledged gold. These documents serve as legal proof of the release and ownership transfer.
                                     </p>
-                                    
+
                                 </div>
                                 <div class="mt-14">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Take Possession of Your Gold:
+                                        Take Possession of Your Gold:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    Upon completing the release process, you will be given possession of your pledged gold, which is rightfully yours. Ensure that you receive a receipt or acknowledgment of the release for your records.
+                                        Upon completing the release process, you will be given possession of your pledged gold, which is rightfully yours. Ensure that you receive a receipt or acknowledgment of the release for your records.
                                     </p>
-                                    
+
                                 </div>
                                 <div class="mt-14">
                                     <h2 class="text-3xl font-bold tracking-tight text-[#FFD700]">
-                                    Repay The Difference Amount:
+                                        Repay The Difference Amount:
                                     </h2>
                                     <p class="mt-4 text-lg text-gray-300">
-                                    After releasing your gold jewellery we evaluate the purity of your gold using advanced technology and pay you the difference amount to your bank account instantly.
+                                        After releasing your gold jewellery we evaluate the purity of your gold using advanced technology and pay you the difference amount to your bank account instantly.
                                     </p>
-                                    
+
                                 </div>
                             </div>
                         </div>
