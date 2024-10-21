7f4d8a2eb2b68270495c2c9c22c77e002b62ebe4
Srinivasan <113420841+Srini09git@users.noreply.github.com>
2024-10-21 11:22:31 +0530

    done

---
+++ src/app/services/cashforgold/page.jsx
@@ -0,0 +1,14 @@
+import CashforgoldX from '@/components/services/cashforgold/CashforgoldX'
+import CashforGoldY from '@/components/services/cashforgold/CashforGoldY'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <CashforGoldY />
+            <CashforgoldX />
+        </div>
+    )
+}
+
+export default page
