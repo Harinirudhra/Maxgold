5f3596729196b21aedb70969f6939454c4cc0a0e
Srinivasan <113420841+Srini09git@users.noreply.github.com>
2024-10-22 16:17:26 +0530

    done

--- src/components/services/ServicesHero.jsx
+++ src/components/services/ServicesHero.jsx
@@ -29,13 +29,13 @@ const ServicesHero = () => {
 
             <div className="absolute inset-0 flex flex-col justify-start lg:justify-center lg:items-start items-center p-4 lg:p-20">
                 <div className="text-center lg:text-left">
-                    <h1 className={` bg-gradient-to-tr from-amber-300 to-yellow-500 text-transparent bg-clip-text text-3xl md:text-3xl pt-12 lg:text-6xl  font-bold transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
-                    Turning Treasures <br/> into Trust </h1> <br />
-                    <h1 className={` bg-gradient-to-tr from-amber-300 to-yellow-500 text-transparent bg-clip-text text-3xl md:text-3xl pt-8 lg:text-6xl  font-bold transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
-                    Service All Over India</h1>
-                   
+                    <h1 className={` bg-gradient-to-tr from-amber-300 to-yellow-500 text-transparent bg-clip-text text-3xl md:text-3xl pt-6 lg:text-6xl  font-bold transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
+                        Turning Treasures <br /> into Trust </h1> <br />
+                    <h1 className={` bg-gradient-to-tr from-amber-300 to-yellow-500 text-transparent bg-clip-text text-3xl md:text-3xl pt-2 lg:text-6xl  font-bold transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
+                        Service All <span className=' block'>Over India</span></h1>
+
                 </div>
-                
+
             </div>
         </div>
     );
